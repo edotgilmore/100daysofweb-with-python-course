f5012eb6295c65cb1c46a122d075f4b192e528a9
edotgilmore <edotgilmore@gmail.com>
2020-09-10 18:44:38 -0600

    Finished day one!

--- days/001-004-flask-intro/code/program/routes.py
+++ days/001-004-flask-intro/code/program/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template
 from program import app
+from datetime import date
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Template Demo', time=timenow)
+    return render_template('index.html', title='Template Demo', time=date.today()) #timenow)
 
 @app.route('/100Days')
 def p100days():
